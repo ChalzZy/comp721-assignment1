c6234f5290056b5f10392d416bed49e306152add
Charles Monaghan <charles@charlesmonaghan.com>
2022-03-29 17:51:05 +1300

    localhost database connection

---
+++ conf/sqlinfo.inc.php
@@ -0,0 +1,12 @@
+<?php
+
+// Include file with sql details
+// please change to your user, password, and db
+// please put it under /home/<user>/conf/
+
+	$sql_host="localhost";
+	$sql_user="root";
+	$sql_pass="";
+	$sql_db="yrw9551";
+?>
+
--- poststatusprocess.php
+++ poststatusprocess.php
@@ -12,4 +12,24 @@
     foreach($checkbox as $item) {
         echo $item . "<br />";
     }
+
+    // include('../conf/sqlinfo.inc.php');
+
+    $sql_host="localhost";
+	$sql_user="root";
+	$sql_pass="";
+	$sql_db="yrw9551";
+
+    $conn = mysqli_connect($sql_host,
+	$sql_user,
+	$sql_pass,
+	$sql_db
+	);
+
+	if (!$conn) {
+		// Displays an error message
+		echo "<p>Database connection failure</p>";
+	} else {
+		echo "<p>Database connection success</p>";
+	}
 ?>
