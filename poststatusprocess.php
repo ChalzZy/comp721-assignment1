8bfbbaf70b62307956148c967854a6e6b39d766f
Charles Monaghan <charles@charlesmonaghan.com>
2022-03-29 22:09:29 +1300

    can now check for duplicate status codes

--- poststatusprocess.php
+++ poststatusprocess.php
@@ -22,12 +22,12 @@
 	);
 
 	if (!$conn) {
-		// Displays an error message
 		echo "<p>Database connection failure</p>";
 	} else {
 		echo "<p>Database connection success</p>";
 	}
 
+    // only creates table if it doesn't already exist
     $sql = "CREATE TABLE IF NOT EXISTS PostStatusTable (
         id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
         statuscode VARCHAR(30) NOT NULL,
@@ -37,9 +37,31 @@
         checkbox VARCHAR(30)
         )";
 
+    $retrieveStatusCode = "SELECT statuscode FROM `poststatustable` 
+        where statuscode like '" . $statusCode . "'";
+
+    // create table
     if ($conn->query($sql) === TRUE) {
         echo "Table MyGuests created successfully";
     } else {
-        echo "Error creating table: ";
+        exit("Error creating table:");
     }
+
+    $noStatusCodeDuplicate = false;
+    // check formating of status code
+    if (strlen($statusCode) != 5 || substr($statusCode, 0, 1) != 'S') {
+        echo "<br />Wrong format! The status code must start with an “S” followed by four digits, like \"S0001\"";
+    } else {
+        // check if the statusCode already exists
+        $result = mysqli_query($conn, $retrieveStatusCode);
+        if (mysqli_num_rows($result) > 0) {
+            exit ("The status code already exists. Please try another one!");
+            $noStatusCodeDuplicate = true;
+        } else {
+            echo "<br />No duplicate status codes found!";
+        }
+    }
+
+    // check formatting of status
+    echo "<br />test";
 ?>
